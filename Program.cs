9f2ea1420522eb14817725427be18f0851adf4c9
Ronny Paulus Muliawan <ronny.muliawan@befutureproof.com>
2017-07-04 12:59:34 +0800

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System.IO;
+
+using Serilog.Core;
+using Serilog.Events;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .UseIISIntegration()
+               .UseStartup<Startup>()
+               .Build();
+
+            host.Run();
+        }
+    }
+
+    public class CustomFilter : ILogEventFilter
+    {
+        public bool IsEnabled(LogEvent logEvent)
+        {
+            return true;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var configuration = new ConfigurationBuilder()
+             .SetBasePath(env.ContentRootPath)
+             .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true)
+             .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+             .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            do
+            {
+                logger.ForContext<Program>().Information("Hello, world!");
+                logger.ForContext(Constants.SourceContextPropertyName, "Microsoft").Warning("Hello, world!");
+                logger.ForContext(Constants.SourceContextPropertyName, "MyApp.Something.Tricky").Verbose("Hello, world!");
+
+                Console.WriteLine();
+            }
+            while (Console.ReadKey().KeyChar != 'q');
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            loggerFactory.AddSerilog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                Console.WriteLine("Hi");
+            });
+        }
+    }
+}
